822f2e1c910dad0f919d3d640f04f15d16228e59
Shinnosuke Dazai <shin@warashi.dev>
2024-06-29 17:53:23 +0900

    wip

--- flake.nix
+++ flake.nix
@@ -19,25 +19,45 @@
       system = "x86_64-linux";
       overlays = [
         (_: prev: {
-          swift = pkgs.callPackage ./swift_5_10.nix {};
+          swift =
+            pkgs.callPackage ./swift_5_10.nix {
+            };
+        })
+        (_: prev: {
           libedit = prev.libedit.overrideAttrs (_: {postInstall ? "", ...}: {
-            postInstall = postInstall + ''
-              cd $out/lib
-              ln -s libedit.so.0 libedit.so.2
-            '';
+            postInstall =
+              postInstall
+              + ''
+                cd $out/lib
+                ln -s libedit.so.0 libedit.so.2
+              '';
           });
         })
       ];
     };
   in {
     devShells.${system}.default = pkgs.mkShell {
-      packages = with pkgs; [swift];
+      packages = with pkgs; [
+        llvmPackages_latest.clang
+        swift
+        cmake
+        fcitx5
+        glslang
+        shaderc
+        vulkan-headers
+        vulkan-loader
+        vulkan-tools
+        glibc
+      ];
     };
     packages.${system} = rec {
       fcitx5-hazkey = pkgs.qt6Packages.callPackage ./fcitx5-hazkey.nix {
         inherit (inputs) hazkey-src;
         inherit (pkgs) fcitx5 swift glslang shaderc vulkan-headers vulkan-loader vulkan-tools;
         inherit (pkgs.llvmPackages_latest) stdenv;
+        curl = pkgs.curl.overrideAttrs (_: old: {
+          configureFlags = old.configureFlags ++ ["--enable-versioned-symbols"];
+        });
       };
       default = fcitx5-hazkey;
     };
--- swift_5_10.nix
+++ swift_5_10.nix
@@ -1,5 +1,5 @@
 {
-  stdenv,
+  pkgs,
   autoPatchelfHook,
   clang,
   curl,
@@ -11,34 +11,43 @@
   libxml2,
   ncurses,
   sqlite,
-}:
-stdenv.mkDerivation {
-  pname = "swift";
-  version = "5.10.1";
+}: let
+  stdenv =
+    pkgs.overrideCC pkgs.stdenv
+    (pkgs.llvmPackages_15.libcxxStdenv.cc.override {
+      inherit (pkgs.llvmPackages_15) bintools;
+    });
+in
+  stdenv.mkDerivation {
+    pname = "swift";
+    version = "5.10.1";
 
-  src = builtins.fetchTarball {
-    url = "https://download.swift.org/swift-5.10.1-release/debian12/swift-5.10.1-RELEASE/swift-5.10.1-RELEASE-debian12.tar.gz";
-    sha256 = "18kg741vvwc5w2yckhfpsnqwp4c30yiz6nzbbgqdg50zmiwr9gm4";
-  };
+    src = builtins.fetchTarball {
+      url = "https://download.swift.org/swift-5.10.1-release/debian12/swift-5.10.1-RELEASE/swift-5.10.1-RELEASE-debian12.tar.gz";
+      sha256 = "18kg741vvwc5w2yckhfpsnqwp4c30yiz6nzbbgqdg50zmiwr9gm4";
+    };
 
-  nativeBuildInputs = [
-    autoPatchelfHook
-  ];
+    nativeBuildInputs = [
+      autoPatchelfHook
+    ];
 
-  propagatedBuildInputs = [
-    clang
-    curl
-    python311
-    libedit
-    libcxx
-    libgcc
-    libuuid
-    libxml2
-    ncurses
-    sqlite
-  ];
+    propagatedBuildInputs = [
+      (curl.overrideAttrs (_: old: {
+        configureFlags = old.configureFlags ++ ["--enable-versioned-symbols"];
+      }))
 
-  buildPhase = ''
-    cp -r $src/usr $out
-  '';
-}
+      clang
+      python311
+      libedit
+      libcxx
+      libgcc
+      libuuid
+      libxml2
+      ncurses
+      sqlite
+    ];
+
+    buildPhase = ''
+      cp -r $src/usr $out
+    '';
+  }
