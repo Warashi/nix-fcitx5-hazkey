df3f75696632cf83af36f25fb41e5fd029485106
Shinnosuke Dazai <shin@warashi.dev>
2024-07-11 22:31:50 +0900

    refactor: remove overlays

--- flake.nix
+++ flake.nix
@@ -11,38 +11,15 @@
     ...
   } @ inputs: let
     system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      system = "x86_64-linux";
-      overlays = [
-        (_: prev: {
-          swift-unwrapped = pkgs.callPackage ./swift-unwrapped_5_10.nix {};
-          swift = pkgs.callPackage ./swiftc_5_10.nix {};
-          swiftpm = pkgs.callPackage ./swiftpm_5_10.nix {};
-          azookey-kkc = pkgs.callPackage ./azookey-kkc.nix {};
-          azookey-kkc-resources = pkgs.callPackage ./azookey-kkc-resources.nix {};
-          fcitx5-hazkey = pkgs.qt6Packages.callPackage ./fcitx5-hazkey.nix {};
-        })
-      ];
-    };
+    pkgs = nixpkgs.legacyPackages.${system};
   in {
-    devShells.${system}.default = pkgs.mkShell {
-      packages = with pkgs; [
-        swift
-        swiftpm
-        cmake
-        gettext
-        fcitx5
-        glslang
-        shaderc
-        vulkan-headers
-        vulkan-loader
-        vulkan-tools
-      ];
-    };
     packages.${system} = rec {
-      fcitx5-hazkey = pkgs.fcitx5-hazkey;
-      azookey-kkc = pkgs.azookey-kkc;
-      azookey-kkc-resources = pkgs.azookey-kkc-resources;
+      swift-unwrapped = pkgs.callPackage ./swift-unwrapped_5_10.nix {};
+      swift = pkgs.callPackage ./swiftc_5_10.nix {inherit swift-unwrapped;};
+      swiftpm = pkgs.callPackage ./swiftpm_5_10.nix {inherit swift-unwrapped;};
+      azookey-kkc-resources = pkgs.callPackage ./azookey-kkc-resources.nix {};
+      azookey-kkc = pkgs.callPackage ./azookey-kkc.nix {inherit swift swiftpm azookey-kkc-resources;};
+      fcitx5-hazkey = pkgs.qt6Packages.callPackage ./fcitx5-hazkey.nix {inherit azookey-kkc;};
       default = fcitx5-hazkey;
     };
   };
