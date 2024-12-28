56b3aa052d0f55d7108b7c23658590cd823e0d50
Shinnosuke Dazai <shin@warashi.dev>
2024-12-28 10:48:46 +0900

    build: update nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,25 +2,31 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgsForSwift.url = "github:nixos/nixpkgs/feb2849fdeb70028c70d73b848214b00d324a497";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  } @ inputs: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    packages.${system} = rec {
-      swift-unwrapped = pkgs.callPackage ./swift-unwrapped_5_10.nix {};
-      swift = pkgs.callPackage ./swiftc_5_10.nix {inherit swift-unwrapped;};
-      swiftpm = pkgs.callPackage ./swiftpm_5_10.nix {inherit swift-unwrapped;};
-      azookey-kkc-resources = pkgs.callPackage ./azookey-kkc-resources.nix {};
-      azookey-kkc = pkgs.callPackage ./azookey-kkc.nix {inherit swift swiftpm azookey-kkc-resources;};
-      fcitx5-hazkey = pkgs.qt6Packages.callPackage ./fcitx5-hazkey.nix {inherit azookey-kkc;};
-      default = fcitx5-hazkey;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgsForSwift,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      pkgsForSwift = nixpkgsForSwift.legacyPackages.${system};
+    in
+    {
+      packages.${system} = rec {
+        swift-unwrapped = pkgsForSwift.callPackage ./swift-unwrapped_5_10.nix {};
+        swift = pkgsForSwift.callPackage ./swiftc_5_10.nix { inherit swift-unwrapped; };
+        swiftpm = pkgsForSwift.callPackage ./swiftpm_5_10.nix { inherit swift-unwrapped; };
+        azookey-kkc-resources = pkgs.callPackage ./azookey-kkc-resources.nix { };
+        azookey-kkc = pkgsForSwift.callPackage ./azookey-kkc.nix { inherit swift swiftpm azookey-kkc-resources; };
+        fcitx5-hazkey = pkgs.qt6Packages.callPackage ./fcitx5-hazkey.nix { inherit azookey-kkc; };
+        default = fcitx5-hazkey;
+      };
     };
-  };
 }
